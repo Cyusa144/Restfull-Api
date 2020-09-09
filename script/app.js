487fb19cdd2f60c958619905cfe3f36689ed7553
Cyusa Irene <cyusa03@gmail.com>
2020-09-09 14:08:26 +0200

    modifies pages to be responsive

--- script/app.js
+++ script/app.js
@@ -1,14 +1,5 @@
-function showNavigation() {
-    var menu = document.getElementById("navigation").style;
-    if (menu.display == 'block') {
-        menu.display= 'none';
-    }else{
-        menu.display='block';
-    }
-}
-var menu_1 = document.getElementById("navigation");
-window.onclick = function(event){
-    if(event.target == menu_1){
-        menu_1.style.display = "none";
-    }
-}
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => { navbarLinks.classList.toggle('active')
+})
