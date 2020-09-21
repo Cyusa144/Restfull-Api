fac05115175e487e91de5859cdb3b6a9a492fa77
Cyusa Irene <cyusa03@gmail.com>
2020-09-21 08:05:23 +0200

    adds new js files and modies features in htmls

---
+++ script/article.js
@@ -0,0 +1,47 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA3pn7TTuOUoGPk3pliVUgbH5-xIysHHj8",
+    authDomain: "cyusa-project-9570e.firebaseapp.com",
+    databaseURL: "https://cyusa-project-9570e.firebaseio.com",
+    projectId: "cyusa-project-9570e",
+    storageBucket: "cyusa-project-9570e.appspot.com",
+    messagingSenderId: "1085067100368",
+    appId: "1:1085067100368:web:9496966a0e62e503f91ed5",
+    measurementId: "G-3X352JZ9HR"
+};
+firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const db = firebase.firestore();
+  const ref = firebase.storage().ref();
+  
+
+let imageLink;
+const coverImage = document.querySelector('.cover-image')
+ coverImage.addEventListener('change',function(){
+   const file =coverImage.files[0]
+   const name = file.name
+  
+   const metadata = {
+     contentType:file.type 
+
+   }
+
+   const task = ref.child(name).put(file,metadata)
+     task.then(snapshot => snapshot.ref.getDownloadURL())
+     .then(url =>{
+       imageLink = url
+     })
+ })
+
+
+  const createArticle=document.querySelector('.create-article');
+  createArticle.addEventListener('submit', function(e){
+    e.preventDefault()
+    db.collection('posts').add({
+      title: createArticle.title.value,
+      body: createArticle.body.value,
+      description: createArticle.description.value,
+      coverImage:imageLink
+
+    })
+ createArticle.reset()
+  })
---
+++ script/posts.js
@@ -0,0 +1,73 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA3pn7TTuOUoGPk3pliVUgbH5-xIysHHj8",
+    authDomain: "cyusa-project-9570e.firebaseapp.com",
+    databaseURL: "https://cyusa-project-9570e.firebaseio.com",
+    projectId: "cyusa-project-9570e",
+    storageBucket: "cyusa-project-9570e.appspot.com",
+    messagingSenderId: "1085067100368",
+    appId: "1:1085067100368:web:9496966a0e62e503f91ed5",
+    measurementId: "G-3X352JZ9HR"
+};
+firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const db = firebase.firestore();
+
+  let link = window.location.href
+  link = link.split('/').reverse()
+  const id = link[0]
+ 
+
+ const imageDiv=document.querySelector('.article-image');
+ let title = document.querySelector('.title');
+ let body = document.querySelector('.body')
+
+
+ function renderArticle(doc){
+  let image = document.createElement('img')
+  image.setAttribute('src',doc.data().coverImage)
+  imageDiv.appendChild(image)
+  title.textContent=doc.data().title
+  body.textContent=doc.data().body
+   
+ }
+ 
+
+  db.collection('posts').doc(id).get().then(doc=>{
+      renderArticle(doc)
+  })
+
+  const addComment=document.querySelector('.form-container');
+  addComment.addEventListener('submit', function(e){
+    e.preventDefault()
+    db.collection('posts').doc(id).collection('comments').add({
+      name: addComment.names.value,
+      email: addComment.email.value,
+      comment: addComment.comment.value,
+      blogId:id
+
+    })
+ addComment.reset()
+  })
+
+  function renderComments(doc){
+     const commentsContainer = document.querySelector('.comments-container')
+     const commentImage = document.createElement('img')
+     commentImage.setAttribute('src','../photos/user.png')
+     let commentName = document.createElement('h3')
+     commentName.textContent=doc.data().name
+     let comment = document.createElement('p')
+     comment.textContent= doc.data().comment
+
+     commentsContainer.appendChild(commentImage)
+     commentsContainer.appendChild(commentName)
+     commentsContainer.appendChild(comment)
+
+  }
+  db.collection('posts').doc(id).collection('comments').get().then((snapshot)=>{
+      //console.log(doc.data())
+      snapshot.docs.forEach(doc => {
+       renderComments(doc)
+        
+      });
+      
+  })
